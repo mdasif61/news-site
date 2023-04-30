7e4ccacae7896b03b6cf9c0873e7a5a82fb50015
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-04-30 13:52:47 +0600

    firebase hide

--- src/components/Layout/firebase/firebase.config.js
+++ src/components/Layout/firebase/firebase.config.js
@@ -1,18 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7iLJnNKBovjTj2gwoOW18AnDby7r7O5A",
-  authDomain: "simple-news-23728.firebaseapp.com",
-  projectId: "simple-news-23728",
-  storageBucket: "simple-news-23728.appspot.com",
-  messagingSenderId: "700540614297",
-  appId: "1:700540614297:web:d31395bbe6b2be90ccaa08"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
